1a27d35ce42d9466a3e71ba3e00ed169ee80792d
Rudy Ravelin <rudy@noukod.com>
2019-03-06 17:08:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+// + Scooter with `year`, `color`, and `model` properties
+function Scooter(year, color, model) {
+  this.year = year
+  this.color = color
+  this.model = model
+}
+
+// + Driver with `name`, `age`, and `experience` properties
+function Driver(name, age, experience) {
+  this.name = name
+  this.age = age
+  this.experience = experience
+}
+
+// + PickupLocation with `address` and `city` properties
+function PickupLocation(address, city) {
+  this.address = address
+  this.city = city
+}
